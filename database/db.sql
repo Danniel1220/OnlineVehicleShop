c3d6acc9a504d8700984d9d1b65390c565d8fd22
george.clim <climdanniel@gmail.com>
2022-02-17 20:04:42 +0200

    Deleted all the individual sql table files in favor of a combined sql file that contains all of them. Also, the user table now contains the test user used in DatabaseConnector's tests.

---
+++ database/db.sql
@@ -0,0 +1,223 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: onlinevehicleshop
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `car`
+--
+
+DROP TABLE IF EXISTS `car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `car` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(45) DEFAULT NULL,
+  `model` varchar(45) DEFAULT NULL,
+  `VIN` varchar(45) DEFAULT NULL,
+  `manufacturingYear` int DEFAULT NULL,
+  `carType` varchar(45) DEFAULT NULL,
+  `engineId` int DEFAULT NULL,
+  `transimissionId` int DEFAULT NULL,
+  `tractionType` varchar(45) DEFAULT NULL,
+  `numberOfDoors` int DEFAULT NULL,
+  `Color` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `engineId` FOREIGN KEY (`id`) REFERENCES `engine` (`id`),
+  CONSTRAINT `transmissionId` FOREIGN KEY (`id`) REFERENCES `transimission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `car`
+--
+
+LOCK TABLES `car` WRITE;
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `electricengine`
+--
+
+DROP TABLE IF EXISTS `electricengine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `electricengine` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) DEFAULT NULL,
+  `batteryCapacity` int DEFAULT NULL,
+  `range` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `electricengine`
+--
+
+LOCK TABLES `electricengine` WRITE;
+/*!40000 ALTER TABLE `electricengine` DISABLE KEYS */;
+/*!40000 ALTER TABLE `electricengine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `engine`
+--
+
+DROP TABLE IF EXISTS `engine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `engine` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fuelEngineId` int DEFAULT NULL,
+  `electricEngineId` int DEFAULT NULL,
+  `horsePower` int DEFAULT NULL,
+  `torque` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `electricEngineId` FOREIGN KEY (`id`) REFERENCES `electricengine` (`id`),
+  CONSTRAINT `fuelEngineId` FOREIGN KEY (`id`) REFERENCES `fuelengine` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `engine`
+--
+
+LOCK TABLES `engine` WRITE;
+/*!40000 ALTER TABLE `engine` DISABLE KEYS */;
+/*!40000 ALTER TABLE `engine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fuelengine`
+--
+
+DROP TABLE IF EXISTS `fuelengine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fuelengine` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fuelType` varchar(45) DEFAULT NULL,
+  `fuelConsumption` float DEFAULT NULL,
+  `numberOfCylinders` int DEFAULT NULL,
+  `CO2Emissions` float DEFAULT NULL,
+  `engineLayout` varchar(45) DEFAULT NULL,
+  `hasTurbine` tinyint DEFAULT NULL,
+  `hasSuperCharger` tinyint DEFAULT NULL,
+  `strokeType` varchar(45) DEFAULT NULL,
+  `displacement` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fuelengine`
+--
+
+LOCK TABLES `fuelengine` WRITE;
+/*!40000 ALTER TABLE `fuelengine` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fuelengine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `available` int DEFAULT NULL,
+  `carId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `carId` FOREIGN KEY (`id`) REFERENCES `car` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transimission`
+--
+
+DROP TABLE IF EXISTS `transimission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transimission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) DEFAULT NULL,
+  `numberOfGears` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transimission`
+--
+
+LOCK TABLES `transimission` WRITE;
+/*!40000 ALTER TABLE `transimission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transimission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'John Doe','123456','client');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-17 20:00:19
