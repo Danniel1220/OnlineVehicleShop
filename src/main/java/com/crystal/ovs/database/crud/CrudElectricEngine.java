6fae007b8dfecfb3b85b80ebdee9e9d52a735bea
Andrei Burlacu <andrei.burlacu@crystal-system.eu>
2022-02-27 13:56:18 +0200

    Update
    Added the conversion of resultset into electric engines. Also added the validations for electric engines

--- src/main/java/com/crystal/ovs/database/crud/CrudElectricEngine.java
+++ src/main/java/com/crystal/ovs/database/crud/CrudElectricEngine.java
@@ -2,7 +2,12 @@ package com.crystal.ovs.database.crud;
 
 import com.crystal.ovs.dao.ElectricEngine;
 import com.crystal.ovs.database.DatabaseConnector;
-import java.sql.*;
+import com.crystal.ovs.exceptions.ValidationException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Contains CRUD operations methods for Electric Engine table
@@ -51,7 +56,43 @@ public class CrudElectricEngine {
         return null;
     }
 
-    public static void insertEngine(ElectricEngine electricEngine) {
+    public static ResultSet selectById(int id) {
+        String query = "SELECT * FROM " + ELECTRIC_ENGINE_TABLE_NAME + " WHERE " + ELECTRIC_ENGINE_ID_COLUMN + " = " + id + ";";
+        try {
+            databaseConnector = DatabaseConnector.getInstance();
+            return databaseConnector.select(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ElectricEngine getElectricEngineFromResultSet(ResultSet resultSet) throws SQLException {
+        return new ElectricEngine (
+                resultSet.getInt(ELECTRIC_ENGINE_ID_COLUMN),
+                resultSet.getString(ELECTRIC_ENGINE_TYPE_COLUMN),
+                resultSet.getInt(ELECTRIC_ENGINE_BATTERY_CAPACITY_COLUMN),
+                resultSet.getInt(ELECTRIC_ENGINE_RANGE_COLUMN)
+        );
+    }
+
+    public static List<ElectricEngine> getAllElectricEngines() throws SQLException {
+        List<ElectricEngine> electricEnginesList = new ArrayList<>();
+
+        ResultSet resultSet = selectAll();
+        while(resultSet.next()) {
+            electricEnginesList.add(getElectricEngineFromResultSet(resultSet));
+        }
+        for(ElectricEngine electricEngine : electricEnginesList) System.out.println(electricEngine.toString());
+        return electricEnginesList;
+    }
+
+    public static void insertEngine(ElectricEngine electricEngine) throws ValidationException {
+        List<String> validationErrors = validateElectricEngine(electricEngine);
+        if (validationErrors.size() > 0) {
+            throw new ValidationException(validationErrors);
+        }
+
         String query = "INSERT INTO " + ELECTRIC_ENGINE_TABLE_NAME +
                 "(" + ELECTRIC_ENGINE_TYPE_COLUMN + ", "
                 + ELECTRIC_ENGINE_BATTERY_CAPACITY_COLUMN + ", "
@@ -65,7 +106,12 @@ public class CrudElectricEngine {
         executeVoidQuery(query);
     }
 
-    public static void updateAllById(ElectricEngine electricEngine) {
+    public static void updateAllById(ElectricEngine electricEngine) throws ValidationException {
+        List<String> validationErrors = validateElectricEngine(electricEngine);
+        if (validationErrors.size() > 0) {
+            throw new ValidationException(validationErrors);
+        }
+
         String query = "UPDATE " + ELECTRIC_ENGINE_TABLE_NAME + " SET " +
                 ELECTRIC_ENGINE_TYPE_COLUMN + " = '" + electricEngine.getType() + "', " +
                 ELECTRIC_ENGINE_BATTERY_CAPACITY_COLUMN + " = " + electricEngine.getBatteryCapacity() + ", " +
@@ -93,5 +139,24 @@ public class CrudElectricEngine {
                 ELECTRIC_ENGINE_RANGE_COLUMN + " = " + range +
                 " WHERE id = " + id + ";";
         executeVoidQuery(query);
+    }
+
+    public static List<String> validateElectricEngine(ElectricEngine electricEngine){
+        List<String> validationErrors = new ArrayList<>();
+
+        if(electricEngine.getId() <= 0) {
+            validationErrors.add("Id cannot be less than or equal to 0");
+        }
+        if(electricEngine.getType().isEmpty()) {
+            validationErrors.add("Electric engine type cannot be empty");
+        }
+        if(electricEngine.getBatteryCapacity() <= 0) {
+            validationErrors.add("Battery capacity cannot be less than or equal to 0");
+        }
+        if(electricEngine.getRange() <= 0) {
+            validationErrors.add("Range cannot be less than or equal to 0");
+        }
+
+        return validationErrors;
     }
 }
