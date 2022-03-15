8e82086ff6555e035122521ce3760bb317b9c77f
Gorzko-Marius <95234833+Gorzko-Marius@users.noreply.github.com>
2022-03-15 13:54:47 +0200

    update CrudUserTest

--- src/test/java/com/crystal/ovs/database/crud/CrudUserTests.java
+++ src/test/java/com/crystal/ovs/database/crud/CrudUserTests.java
@@ -27,11 +27,22 @@ public class CrudUserTests {
     @Test
     public void insertUserTest() { // works, after you make the select method you can properly add assertions in this test
         User user = new User(0, "client", "client", UserRole.CLIENT, "client@gmail.com");
-        CrudUser.insertUser(user);
+        Assert.assertEquals(1, CrudUser.insertUser(user));
     }
 
     @Test
-    public void selectAdminUserTest() {
+    public void updateUserTest() {
+        User user = new User(0, "client21", "client", UserRole.CLIENT, "client@gmail.com");
+        Assert.assertEquals(1, CrudUser.updateUser(user));
+    }
+
+    @Test
+    public void deleteUserTest() {
+        Assert.assertEquals(1, CrudUser.deleteUser(1));
+    }
+
+    @Test
+    public void selectUserByCredentialsTest() {
         User user;
         try {
             user = CrudUser.selectUserByCredentials("admin@gmail.com", "admin");
@@ -46,6 +57,34 @@ public class CrudUserTests {
     }
 
     @Test
+    public void selectAllAdminUsersTest() {
+        List<User> userList;
+        userList = CrudUser.selectAllAdminUsers();
+        int nrOfRows = CrudUser.getNumberOfRows();
+        Assert.assertEquals(nrOfRows, userList.size());
+        for (User user : userList) {
+            Assert.assertEquals(UserRole.ADMIN,user.getRole());
+        }
+    }
+
+    @Test
+    public void selectAllClientUsers() {
+        List<User> userList;
+        userList = CrudUser.selectAllClientUsers();
+        int nrOfRows = CrudUser.getNumberOfRows();
+        Assert.assertEquals(nrOfRows, userList.size());
+        for (User user : userList) {
+            Assert.assertEquals(UserRole.CLIENT,user.getRole());
+        }
+    }
+
+    @Test
+    public void selectUserByIdTest() {
+        User user = CrudUser.selectUserById(1);
+        Assert.assertNotNull(user);
+    }
+
+    @Test
     public void selectMissingAdminUserTest() {
         User user;
         try {
@@ -56,8 +95,5 @@ public class CrudUserTests {
         }
     }
 
-    @Test
-    public void createAlreadyExistingAccount() {
-        User user = new User(0, "client", "client", UserRole.CLIENT, "client@gmail.com");
-    }
+    
 }
